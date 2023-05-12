7c205176d3e82795d07a097f6b8d0870a108c515
nobelrakib03 <nobelrakib>
2023-05-12 20:56:22 +0800

    basket checkout microservice with cdk,eventbridge

--- aws-microservices/lib/aws-microservices-stack.ts
+++ aws-microservices/lib/aws-microservices-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { SwnApiGateway } from './apigateway';
 import { SwnDatabase } from './database';
+import { SwnEventBus } from './eventbus';
 import { SwnMicroservices } from './microservice';
 
 export class AwsMicroservicesStack extends Stack {
@@ -12,12 +13,19 @@ export class AwsMicroservicesStack extends Stack {
 
     const microservices = new SwnMicroservices(this, 'Microservices', {
       productTable: database.productTable,
-      basketTable: database.basketTable
+      basketTable: database.basketTable,
+      orderTable: database.orderTable
     });
 
     const apigateway = new SwnApiGateway(this, 'ApiGateway', {
       productMicroservice: microservices.productMicroservice,
-      basketMicroservice: microservices.basketMicroservice
-    });    
+      basketMicroservice: microservices.basketMicroservice,
+      orderingMicroservices: microservices.orderingMicroservice
+    });
+    
+    const eventbus = new SwnEventBus(this, 'EventBus', {
+      publisherFuntion: microservices.basketMicroservice,
+      targetFuntion: microservices.orderingMicroservice
+    });
   }
 }
